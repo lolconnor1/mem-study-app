c6eef69d8b7b965ee0977812e74f434fbbde4438
lolconnor1 <connorchan88@gmail.com>
2025-03-17 21:19:35 -0400

    added post-module stats page

--- app/src/main/java/com/example/finalproj/moduleStats.java
+++ app/src/main/java/com/example/finalproj/moduleStats.java
@@ -1,6 +1,10 @@
 package com.example.finalproj;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class moduleStats extends AppCompatActivity {
+
+    TextView finalTime, finalCorrect, method, module;
+    Button homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,36 @@ public class moduleStats extends AppCompatActivity {
             return insets;
         });
 
+        Intent i = getIntent();
+        int total = i.getIntExtra("total", 0);
+        int correct = i.getIntExtra("correct", 0);
+        int seconds = i.getIntExtra("time", 0);
+        int modNum = i.getIntExtra("module", 0);
+        String methodstr = i.getStringExtra("method");
+
+        finalTime = (TextView) findViewById(R.id.finalTime);
+        finalCorrect = (TextView) findViewById(R.id.finalCorrect);
+        method = (TextView) findViewById(R.id.method);
+        module = (TextView) findViewById(R.id.module);
+
+        finalCorrect.setText("Correct Cards: " + correct + "/" + total);
+        method.setText("Method: " + methodstr);
+        module.setText("Module: Danish " + modNum);
+
+        int minutes = seconds / 60;
+        int sec = seconds % 60;
+        String timeFormatted = String.format("%02d:%02d", minutes, sec);
+        finalTime.setText("Time Elapsed: " + timeFormatted);
+
+        homeButton = (Button) findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(moduleStats.this, moduleView.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
